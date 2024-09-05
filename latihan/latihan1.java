25693a6c03e2f790ed6e9243d61665f61a5f0e90
felicia <2381064@unai.edu>
2024-09-05 22:09:20 +0700

    HANDSON

--- latihan/latihan1.java
+++ latihan/latihan1.java
@@ -2,6 +2,22 @@ package latihan;
 
 public class latihan1 {
     public static void main(String[] args) {
-        int[] bilangan = new int[3];
+
+        int[] bilangan = new int[2];
+        Scanner input = new Scanner(System.in);
+
+
+        for (int i = 0; i < bilangan.length; i++) {
+            System.out.print("Masukan Bilangan ke - " + i + " : ");
+            bilangan[i] = input.nextInt();
+        }
+
+        int largestNum = bilangan[0];
+        for (int i = 1; i < bilangan.length; i++) {
+            if (largestNum < bilangan[i]) {
+                largestNum = bilangan[i];
+            }
+        }
+        System.out.println(largestNum);
     }
 }
